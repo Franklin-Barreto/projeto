ae88471069b8b76f2ef6fe4d58d2423b8ed071d2
Franklin Barreto <franklin-barreto@hotmail.com>
2019-10-22 23:01:00 +0100

    first commit

---
+++ backend/foodexpress/src/test/java/br/com/foodexpress/FoodexpressApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.foodexpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodexpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
